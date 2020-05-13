159072689d2a116bf252d372bebd7148e591e2da
Nicolas Moreno Duran <morenodurann@gmail.com>
2020-05-12 21:17:39 -0500

    Martes 12 de mayo

---
+++ RecargaPlus/RecargaPlus/Program.cs
@@ -0,0 +1,23 @@
+﻿using RecargaPlus.src.views.pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecargaPlus
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login_page());
+        }
+    }
+}
---
+++ RecargaPlus/RecargaPlus/src/views/pages/Login_page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecargaPlus.src.views.pages
+{
+    public partial class Login_page : Form
+    {
+        public Login_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
